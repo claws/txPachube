319d7f6c3369a2e679663a82c4cfebdcdebab522
Chris Laws <clawsicus@gmail.com>
2012-01-02 14:59:44 +1030

    initial upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+sdict = {
+    'name' : 'txpachube',
+    'version' : '0.1',
+    'description' : 'Python/Twisted wrapper of Pachube API',
+    'author' : 'Chris Laws',
+    'author_email' : 'clawsicus@gmail.com',
+    'url' : 'https://github.com/claws/txPachube'
+    'packages' : ['txpachube'],
+    'classifiers' : [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Twisted',
+        ],
+}
+
+from setuptools import setup
+setup(**sdict)
